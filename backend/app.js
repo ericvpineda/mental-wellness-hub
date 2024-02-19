3eee5881bf4354836dbc9f5362cdea878208089f
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-02-19 13:12:26 -0800

    testing pipeline

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
+// Test backend
 
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World2');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Test
