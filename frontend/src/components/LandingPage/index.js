02ff0075fc758ce461afbf190ae49fbeff082021
sfazli96 <sfazli96@gmail.com>
2024-02-20 23:39:51 -0800

    added some paragraph in landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -6,6 +6,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Welcome to our Mental Health </h1>
+            <h2>Easy to use Navigation for writing down your thoughts in the journal about mental health</h2>
             <p></p>
             <p>Footer below</p>
         </div>
