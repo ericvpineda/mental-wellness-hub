ab8366a08d4e90aec568ddb56f0c26ca5bcf3b50
Eric Pineda <ericvpineda@berkeley.edu>
2024-02-24 21:05:15 -0800

    updated -- added createBrowserRouter for frontend routes

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,10 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="App">
+      Home Page
+    </div>
+  );
+}
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./components/Home";
+import Layout from "./Layout";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Journal from "./routes/Journal";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "journal",
+        element: <Journal />,
+      },
+    ],
+  },
+]);
 
+const root = createRoot(document.getElementById("root"))
+root.render(<RouterProvider router={router} />);
