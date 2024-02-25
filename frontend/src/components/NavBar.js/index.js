677bd1e2ad1903ae0c4ab2d67c418cfc06f5c8b1
sfazli96 <sfazli96@gmail.com>
2024-02-25 15:12:08 -0800

    add faq and contact-us on navbar

--- frontend/src/components/NavBar.js/index.js
+++ frontend/src/components/NavBar.js/index.js
@@ -3,11 +3,13 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav>
-            <ul className="flex justify-between p-4 bg-blue-500 text-white">
+            <ul className="flex justify-end p-4 bg-blue-500 text-white">
                 <ul className="flex">
                     <li className="mx-4 hover:bg-sky-700"><a href="/">Home</a></li>
-                    <li className="mx-4 hover:bg-sky-700"><a href="/resources">Resources</a></li>
                     <li className="mx-4 hover:bg-sky-700"><a href="/about-us">About Us</a></li>
+                    <li className="mx-4 hover:bg-sky-700"><a href="/resources">Resources</a></li>
+                    <li className="mx-4 hover:bg-sky-700"><a href="/faq">Faq</a></li>
+                    <li className="mx-4 hover:bg-sky-700"><a href="/contact-us">Contact Us</a></li>
                 </ul>
             </ul>
         </nav>
