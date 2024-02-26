92ea681a06516f9933ad3fce9c2cce1fb7bd1582
sfazli96 <sfazli96@gmail.com>
2024-02-25 16:18:22 -0800

    fixed some tailwindcss

--- frontend/src/routes/LandingPage/index.js
+++ frontend/src/routes/LandingPage/index.js
@@ -10,8 +10,8 @@ const HomePage = () => {
             <h1 className="text-3xl font-bold mt-8">Here is a small demo on how it works</h1>
             <div className="relative mt-8">
                 <div className="w-64 h-64 rounded-full bg-blue-500 opacity-50 absolute animate-pulse"></div>
-                {/* <div className="w-64 h-64 rounded-full bg-blue-500 opacity-25 absolute animate-ping"></div> */}
-                <div className="w-64 h-64 rounded-full bg-blue-500 absolute flex items-center justify-center">
+                <div className="w-64 h-64 rounded-full bg-blue-500 opacity-25 absolute animate-ping"></div>
+                <div className="w-64 h-64 rounded-full bg-blue-500 flex items-center justify-center">
                     <span className="text-white text-3xl font-bold">information here</span>
                 </div>
             </div>
@@ -26,26 +26,6 @@ const HomePage = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
             </div>
-
-        <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-            </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </footer>
-
         </div>
     )
 }
