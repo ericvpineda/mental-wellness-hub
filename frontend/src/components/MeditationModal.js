5385fb5962b3c4d5e2d9f4d5cbb3d85ccbe93f99
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-03-08 17:19:08 -0800

    adding modal and customizing it in CSS

--- frontend/src/components/MeditationModal.js
+++ frontend/src/components/MeditationModal.js
@@ -1,13 +1,32 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-export default function MeditationModal() {
-    return(
-        <div className="h-full w-full absolute rounded-md flex justify-center items-center">
-            <div className="bg-black w-1/2 h-2/3 rounded-md">
-                <h1>Great Meditation!</h1>
-                <h1>1-10</h1>
-                <h1>General Description</h1>
+export default function MeditationModal( {toggleModal} ) {
+    return (
+        <div className="h-full w-full text-black fixed rounded-md flex justify-center items-center bg-opacity-50 bg-black">
+            <div className="bg-white w-3/4 md:w-1/2 lg:w-1/3 h-2/3 border-2 border-gray-300 flex flex-col items-center justify-around p-4 shadow-lg rounded-lg">
+                <div className="w-full flex justify-center items-center">
+                    <h1 className="text-4xl font-bold text-gray-700">Great Meditation!</h1>
+                </div>
+                <div className="w-full flex flex-col items-center justify-around text-black">
+                    <div className="w-full flex flex-col justify-around items-center mb-4">
+                        <label className="text-xl font-semibold text-gray-700" htmlFor="meditationRange">Rate Your Meditation</label>
+                        <input type="range" min="1" max="10" step="1" className="slider mt-2" />
+                    </div>
+                    <div className="w-full flex flex-col justify-around items-center mb-4">
+                        <h1 className="text-xl font-semibold text-gray-700">What did you learn?</h1>
+                        <textarea className="text-black mt-2 p-2 w-full h-24 border rounded"></textarea>
+                    </div>
+                    <div className="w-1/2 flex justify-around items-center">
+                        <button onClick={toggleModal} className="py-2 px-4 bg-gray-300 hover:bg-gray-400 rounded-md text-black">Close</button>
+                        <button onClick={toggleModal} className="py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-md text-white">Submit</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
+
+MeditationModal.propTypes = {
+    toggleModal: PropTypes.func.isRequired,
+};
--- frontend/src/routes/Meditation.js
+++ frontend/src/routes/Meditation.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import MeditationModal from '../components/MeditationModal';
 import meditation from '../assets/meditation.mp3';
 
+
 export default function Meditation() {
     const audioRef1 = useRef(null);
 
@@ -11,7 +12,7 @@ export default function Meditation() {
     const [minutesLeft, setMinutesLeft] = useState("00");
     const [secondsLeft, setSecondsLeft] = useState("00");
     const [inputValue, setInputValue] = useState();
-    const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(true);
 
     const togglePlay = (audioRef) => {
         if (audioRef.current.paused) {
@@ -21,6 +22,11 @@ export default function Meditation() {
             audioRef.current.pause();
             setIsPlaying(false);
         }
+    }
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+        console.log(showModal);
     }
 
     const startTimer = () => {
@@ -58,44 +64,44 @@ export default function Meditation() {
 
   return (
     <div className="h-screen">
-    {showModal && <MeditationModal />}
-    <div className="h-screen">
-        <div className={"flex flex-col items-center justify-center h-3/4 bg-white"}>
-            <div>
-                <h1 className="text-7xl font-bold mb-2">Meditation Timer</h1>
-                <p className="text-3xl text-center text-gray-500 mb-4">Set your intentions and peace.</p>
+        {showModal && <MeditationModal toggleModal={toggleModal}/>}
+        <div className="h-screen">
+            <div className={"flex flex-col items-center justify-center h-3/4 bg-white"}>
+                <div>
+                    <h1 className="text-7xl font-bold mb-2">Meditation Timer</h1>
+                    <p className="text-3xl text-center text-gray-500 mb-4">Set your intentions and peace.</p>
+                </div>
+                <div className="flex justify-center items-center w-1/6">
+                    <ClockIcon className="w-6 h-6 mr-2" />
+                    <form className="w-2/3" onSubmit={handleSubmit}>
+                        <input type="number" min="0" max="100" className="w-full p-2 border-2 border-gray-300 rounded-lg" value={inputValue} onChange={e => setInputValue(e.target.value)} disabled={timerActive} placeholder="Enter minutes" />
+                    </form>
+                </div>
+                <div className="w-1/6 flex justify-around mt-4">
+                    <button name="start" onClick={ startTimer } className="bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 hover:text-sky-500 duration-200">Start</button>
+                    <button className="bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 ml-4 hover:text-sky-500 duration-200">Stop</button>
+                </div>
+                <div className={timerActive ? "block" : "hidden"}>
+                    <h1 className="text-3xl font-bold mt-4 text-center">Time Remaining:</h1>
+                    <h1 className="text-7xl font-bold text-center">{hoursLeft}:{minutesLeft}:{secondsLeft}</h1>
+                </div>
             </div>
-            <div className="flex justify-center items-center w-1/6">
-                <ClockIcon className="w-6 h-6 mr-2" />
-                <form className="w-2/3" onSubmit={handleSubmit}>
-                    <input type="number" min="0" max="100" className="w-full p-2 border-2 border-gray-300 rounded-lg" value={inputValue} onChange={e => setInputValue(e.target.value)} disabled={timerActive} placeholder="Enter minutes" />
-                </form>
+            <div className={"flex flex-col h-1/4 justify-around items-center border-t-2"}>
+            <h1 className="text-2xl font-bold">Peaceful, ad-free music for zero disturbance.</h1>
+            <div className="flex flex-row w-1/2 justify-around">
+                    <div className="flex flex-row items-center bg-gradient-to-r from-purple-500 to-pink-500 px-6 py-3 rounded-md">
+                        <h1 className="text-lg font-semibold">Ambient Music</h1>
+                        <button onClick={() => togglePlay(audioRef1)} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-green-400 duration-200">{isPlaying ? 'Pause' : 'Play'}</button>
+                        <audio ref={audioRef1} src={meditation}/>
+                    </div>
+                    <div className="flex flex-row items-center bg-tree px-6 py-3 rounded-md">
+                        <h1 className="text-lg font-semibold">Nature Sounds</h1>
+                        <button onClick={() => togglePlay(audioRef1)} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-green-400 duration-200">{isPlaying ? 'Pause' : 'Play'}</button>
+                        <audio ref={audioRef1} src={meditation}/>
+                    </div>
             </div>
-            <div className="w-1/6 flex justify-around mt-4">
-                <button name="start" onClick={ startTimer } className="bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 hover:text-sky-500 duration-200">Start</button>
-                <button className="bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 ml-4 hover:text-sky-500 duration-200">Stop</button>
-            </div>
-            <div className={timerActive ? "block" : "hidden"}>
-                <h1 className="text-3xl font-bold mt-4 text-center">Time Remaining:</h1>
-                <h1 className="text-7xl font-bold text-center">{hoursLeft}:{minutesLeft}:{secondsLeft}</h1>
             </div>
         </div>
-        <div className={"flex flex-col h-1/4 justify-around items-center border-t-2"}>
-           <h1 className="text-2xl font-bold">Peaceful, ad-free music for zero disturbance.</h1>
-           <div className="flex flex-row w-1/2 justify-around">
-                <div className="flex flex-row items-center bg-gradient-to-r from-purple-500 to-pink-500 px-6 py-3 rounded-md">
-                    <h1 className="text-lg font-semibold">Ambient Music</h1>
-                    <button onClick={() => togglePlay(audioRef1)} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-green-400 duration-200">{isPlaying ? 'Pause' : 'Play'}</button>
-                    <audio ref={audioRef1} src={meditation}/>
-                </div>
-                <div className="flex flex-row items-center bg-tree px-6 py-3 rounded-md">
-                    <h1 className="text-lg font-semibold">Nature Sounds</h1>
-                    <button onClick={() => togglePlay(audioRef1)} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-green-400 duration-200">{isPlaying ? 'Pause' : 'Play'}</button>
-                    <audio ref={audioRef1} src={meditation}/>
-                </div>
-           </div>
-        </div>
-    </div>
     </div>
   )
 }
