26b304a447139100106a0a9cffeedd66a856d71c
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-03-13 12:22:53 -0700

    adding audio and toggle button

--- frontend/src/routes/Meditation.js
+++ frontend/src/routes/Meditation.js
@@ -5,29 +5,37 @@ import meditation from '../assets/meditation.mp3';
 
 export default function Meditation() {
     const audioRef1 = useRef(null);
+    const audioRef2 = useRef(null);
 
-    const [isPlaying, setIsPlaying] = useState(false);
+    const [isPlaying, setIsPlaying] = useState({audio1: false, audio2: false});
     const [timerActive, setTimerActive] = useState(false);
     const [hoursLeft, setHoursLeft] = useState("00");
     const [minutesLeft, setMinutesLeft] = useState("00");
     const [secondsLeft, setSecondsLeft] = useState("00");
     const [inputValue, setInputValue] = useState();
-    const [showModal, setShowModal] = useState(true);
+    const [showModal, setShowModal] = useState(false);
 
-    const togglePlay = (audioRef) => {
+    const togglePlay = (audioRef, audioKey) => {
         if (audioRef.current.paused) {
+            // Pause the other audio
+            if (audioKey === 'audio1') {
+                audioRef2.current.pause();
+                setIsPlaying(prevState => ({...prevState, audio2: false}));
+            } else {
+                audioRef1.current.pause();
+                setIsPlaying(prevState => ({...prevState, audio1: false}));
+            }
+    
+            // Play the current audio
             audioRef.current.play();
-            setIsPlaying(true);
+            setIsPlaying(prevState => ({...prevState, [audioKey]: true}));
         } else {
             audioRef.current.pause();
-            setIsPlaying(false);
+            setIsPlaying(prevState => ({...prevState, [audioKey]: false}));
         }
     }
 
-    const toggleModal = () => {
-        setShowModal(!showModal);
-        console.log(showModal);
-    }
+    const intervalRef = useRef(null);
 
     const startTimer = () => {
         setTimerActive(true);
@@ -38,7 +46,7 @@ export default function Meditation() {
         setHoursLeft(hours.toString().padStart(2, '0'));
         setMinutesLeft(minutes.toString().padStart(2, '0'));
         setSecondsLeft(seconds.toString().padStart(2, '0'));
-        const interval = setInterval(() => {
+        intervalRef.current = setInterval(() => {
             if (time > 0) {
                 time--;
                 hours = Math.floor(time / 3600);
@@ -48,12 +56,25 @@ export default function Meditation() {
                 setMinutesLeft(minutes.toString().padStart(2, '0'));
                 setSecondsLeft(seconds.toString().padStart(2, '0'));
             } else {
-                clearInterval(interval);
+                clearInterval(intervalRef);
                 setTimerActive(false);
                 setShowModal(true);
             }
         }, 1000);
     }
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+        console.log(showModal);
+        clearInterval(intervalRef.current);
+    }
+
+    const stopTimer = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            setTimerActive(false);
+        }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevents the page from refreshing when the form is submitted
@@ -78,8 +99,8 @@ export default function Meditation() {
                     </form>
                 </div>
                 <div className="w-1/6 flex justify-around mt-4">
-                    <button name="start" onClick={ startTimer } className="bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 hover:text-sky-500 duration-200">Start</button>
-                    <button className="bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 ml-4 hover:text-sky-500 duration-200">Stop</button>
+                    <button id="timerButton" name="start" onClick={ startTimer } className={timerActive ? "hidden" : "bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 hover:text-green-500 duration-200"}>Start</button>
+                    <button onClick={ stopTimer } className={timerActive ? "bg-black border-2 border-black text-white font-bold py-2 px-6 rounded-md mt-4 ml-4 hover:text-red-500 duration-200" : "hidden"}>Stop</button>
                 </div>
                 <div className={timerActive ? "block" : "hidden"}>
                     <h1 className="text-3xl font-bold mt-4 text-center">Time Remaining:</h1>
@@ -91,13 +112,13 @@ export default function Meditation() {
             <div className="flex flex-row w-1/2 justify-around">
                     <div className="flex flex-row items-center bg-gradient-to-r from-purple-500 to-pink-500 px-6 py-3 rounded-md">
                         <h1 className="text-lg font-semibold">Ambient Music</h1>
-                        <button onClick={() => togglePlay(audioRef1)} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-green-400 duration-200">{isPlaying ? 'Pause' : 'Play'}</button>
+                        <button onClick={() => togglePlay(audioRef1, 'audio1')} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-sky-500 duration-200">{isPlaying.audio1 ? 'Pause' : 'Play'}</button>
                         <audio ref={audioRef1} src={meditation}/>
                     </div>
                     <div className="flex flex-row items-center bg-tree px-6 py-3 rounded-md">
                         <h1 className="text-lg font-semibold">Nature Sounds</h1>
-                        <button onClick={() => togglePlay(audioRef1)} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-green-400 duration-200">{isPlaying ? 'Pause' : 'Play'}</button>
-                        <audio ref={audioRef1} src={meditation}/>
+                        <button onClick={() => togglePlay(audioRef2, 'audio2')} className="ml-4 bg-black text-white rounded py-2 px-4 hover:text-sky-500 duration-200">{isPlaying.audio2 ? 'Pause' : 'Play'}</button>
+                        <audio ref={audioRef2} src={meditation}/>
                     </div>
             </div>
             </div>
