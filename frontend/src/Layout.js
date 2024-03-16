23ca2f982e2ee4e6107394d1cd60cd64f8b23b02
sfazli96 <sfazli96@gmail.com>
2024-03-15 17:21:13 -0700

    fixed footer

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -8,20 +8,20 @@ export default function Layout() {
       <Navbar />
       <Outlet />
       <footer className="z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                    <a href="/about" className="hover:underline me-4 md:me-6">About</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="/resources" className="hover:underline me-4 md:me-6">Resources</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                    <a href="/faq" className="hover:underline me-4 md:me-6">Faq</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <a href="/contact-us" className="hover:underline">Contact</a>
                 </li>
             </ul>
         </footer>
