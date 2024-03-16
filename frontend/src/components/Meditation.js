c71adc8a9f1f7e798e8e6cdba68921fbccf3e2d1
sfazli96 <sfazli96@gmail.com>
2024-03-15 17:25:11 -0700

    fixing pr request merge conflict

--- frontend/src/components/Meditation.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Meditation() {
-    return (
-        <div className="w-full bg-neutral-300">
-            <h1>Test</h1>
-        </div>
-    )
-}
